1ff3ab2d7b27c4ef6ae7e1b32dd5e52525d36fa0
moleculoman <Xz2@ZBS@mhSFpg7>
2023-04-23 20:15:44 +0700

    initial commit

---
+++ src/test/java/com/example/homew3_testingmockito/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homew3_testingmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
