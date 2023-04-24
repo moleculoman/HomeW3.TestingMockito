0f5bdbacd770ef51a4eb760715b93183bc9eae45
moleculoman <Xz2@ZBS@mhSFpg7>
2023-04-24 21:27:38 +0700

    test_start_fix_v2

--- src/test/java/com/example/homew3_testingmockito/DAO/UserDaoImplTest.java
+++ src/test/java/com/example/homew3_testingmockito/DAO/UserDaoImplTest.java
@@ -1,5 +1,6 @@
 package com.example.homew3_testingmockito.DAO;
 
+import com.example.homew3_testingmockito.Model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ public class UserDaoImplTest {
 
     @Test
     void getUserByName() {
-        Assertions.assertNotNull(userDao.getUserByName("Петр"));
+        Assertions.assertNotNull(userDao.getUserByName("Алексей"));
     }
 
     @Test
